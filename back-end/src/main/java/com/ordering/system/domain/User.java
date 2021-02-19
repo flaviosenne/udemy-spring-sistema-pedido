4f834357e77382e2eb8e4aefadfdd834cc59d504
flaviosenne123 <joaoflausino123@hotmail.com>
2021-02-19 19:43:13 -0300

    mapping class domain with JPA

---
+++ back-end/src/main/java/com/ordering/system/domain/User.java
@@ -0,0 +1,23 @@
+package com.ordering.system.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    private String email;
+}
