d9158abb72eef8e276623e66bd8df3b8e69536c9
flaviosenne123 <joaoflausino123@hotmail.com>
2021-03-01 20:26:20 -0300

    email working

--- back-end/src/main/java/com/ordering/system/services/RequestsService.java
+++ back-end/src/main/java/com/ordering/system/services/RequestsService.java
@@ -29,8 +29,8 @@ public class RequestsService {
     private RequestItemRepository requestItemRepository;
     @Autowired
     private ClientRepository clientRepository;
-    // @Autowired
-    // private SendEmailService sendEmailService;
+    @Autowired
+    private SendEmailService sendEmailService;
 
     public ResponseEntity<Requests> getRequestById(Integer id){
         Optional<Requests> request = this.requestsRepository.findById(id);
@@ -64,7 +64,7 @@ public class RequestsService {
             item.setRequest(requests);
         }
         this.requestItemRepository.saveAll(requests.getItens());
-        // this.sendEmailService.sendOrderConfirmationEmail(requests);
+        this.sendEmailService.sendOrderConfirmationEmail(requests);
         return requests;
     }
 }
--- back-end/src/main/java/com/ordering/system/services/SendEmailService.java
+++ back-end/src/main/java/com/ordering/system/services/SendEmailService.java
@@ -1,49 +1,50 @@
-// package com.ordering.system.services;
+package com.ordering.system.services;
 
-// import java.util.Date;
+import java.util.Date;
 
-// import com.ordering.system.domains.Requests;
+import com.ordering.system.domains.Requests;
 
 
-// import org.slf4j.Logger;
-// import org.slf4j.LoggerFactory;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.beans.factory.annotation.Value;
-// import org.springframework.mail.MailSender;
-// import org.springframework.mail.SimpleMailMessage;
-// import org.springframework.stereotype.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Component;
 
-// @Component
-// public class SendEmailService {
+@Component
+public class SendEmailService {
     
-//     @Autowired
-//     private MailSender mailSender;
+    @Autowired
+    private MailSender mailSender;
     
-//     @Value("${default.sender}")
-//     private String sender;
+    @Value("${default.sender}")
+    private String sender;
 
-//     private static final Logger LOG =  LoggerFactory.getLogger(SendEmailService.class);
+    private static final Logger LOG =  LoggerFactory.getLogger(SendEmailService.class);
 
-//     public void sendEmail(SimpleMailMessage msg){
-//         LOG.info("envio de email");
-//         mailSender.send(msg);
-//         LOG.info("Email Enviado");
-//     }
+    public void sendEmail(SimpleMailMessage msg){
+        LOG.info("envio de email");
+        mailSender.send(msg);
+        LOG.info("Email Enviado");
+    }
 
-//     public void sendOrderConfirmationEmail(Requests requests){
+    public void sendOrderConfirmationEmail(Requests requests){
 
-//         SimpleMailMessage sm = prepareSimpleMailMesageFromRequests(requests);
-//         sendEmail(sm);
-//     }
+        SimpleMailMessage sm = prepareSimpleMailMesageFromRequests(requests);
+        sendEmail(sm);
+    }
 
-//     protected SimpleMailMessage prepareSimpleMailMesageFromRequests(Requests requests){
-//         SimpleMailMessage sm = new SimpleMailMessage();
-//         sm.setTo(requests.getClient().getEmail());
-//         sm.setFrom(this.sender);
-//         sm.setSubject("Pedido Confirmado cod: "+ requests.getId());
-//         sm.setSentDate(new Date(System.currentTimeMillis()));
-//         sm.setText(requests.toString());
+    protected SimpleMailMessage prepareSimpleMailMesageFromRequests(Requests requests){
+        SimpleMailMessage sm = new SimpleMailMessage();
+        sm.setTo(requests.getClient().getEmail());
+        sm.setFrom(this.sender);
+        sm.setSubject("Pedido Confirmado cod: "+ requests.getId());
+        sm.setSentDate(new Date(System.currentTimeMillis()));
+        sm.setText(requests.toString());
         
-//         return sm;
-//     }
-// }
+        System.out.println(sm);
+        return sm;
+    }
+}
