aea61812acb81ee33deb9c16c23b919876d60ca5
flaviosenne123 <joaoflausino123@hotmail.com>
2021-03-18 22:22:18 -0300

    menu disable in route home

--- front/src/app/pages/home/home.ts
+++ front/src/app/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {  NavController } from '@ionic/angular';
+import { NavController, MenuController } from '@ionic/angular';
 
 @Component({
     selector: 'page-home',
@@ -9,12 +9,17 @@ import {  NavController } from '@ionic/angular';
 
 export class HomePage {
 
-    constructor(public navCtrl: NavController){
-
-    }
+    constructor(public navCtrl: NavController, public menu: MenuController){}
 
     login(){
         this.navCtrl.navigateBack('categories')
+    }
+
+    ionViewWillEnter(){
+        this.menu.swipeGesture(false)
+    }
+    ionViewDidLeave(){
+        this.menu.swipeGesture(true)
     }
     
 }
