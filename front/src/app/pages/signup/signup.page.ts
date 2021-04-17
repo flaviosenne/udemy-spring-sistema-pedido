ad3fd028984b91755bed80ddcdbde7c081c694f9
Joao flavio <flaviosenne123@gmail.com>
2021-04-17 14:16:16 -0300

    message validation in view in fileds required

--- front/src/app/pages/signup/signup.page.ts
+++ front/src/app/pages/signup/signup.page.ts
@@ -18,18 +18,18 @@ export class SignupPage implements OnInit {
     private http: HttpClient) { 
 
       this.formGroup = this.formBuilder.group({
-        name: ['Joao',[Validators.required, Validators.minLength(5),Validators.maxLength(120)]],
-        email: ['joao@email.com',[Validators.required, Validators.email]],
-        password: ['123',[Validators.required, Validators.minLength(5),Validators.maxLength(120)]],
-        type: ['1',[Validators.required]],
-        cpfOrCnpj: ['111.222.333-44',[Validators.required, Validators.minLength(11), Validators.maxLength(14)]],
-        phone1: ['12345678910',[Validators.required, Validators.maxLength(11)]],
+        name: ['',[Validators.required, Validators.minLength(5),Validators.maxLength(120)]],
+        email: ['',[Validators.required, Validators.email]],
+        password: ['',[Validators.required, Validators.minLength(5),Validators.maxLength(120)]],
+        type: ['',[Validators.required]],
+        cpfOrCnpj: ['',[Validators.required, Validators.minLength(11), Validators.maxLength(14)]],
+        phone1: ['',[Validators.required, Validators.maxLength(11)]],
         phone2: ['',[]],
-        place: ['rua ameda',[Validators.required]],
-        number: ['1234',[Validators.required]],
-        district: ['Aviação',[Validators.required]],
+        place: ['',[Validators.required]],
+        number: ['',[Validators.required]],
+        district: ['',[Validators.required]],
         complement: ['',[]],
-        postalCode: ['1111222',[Validators.required]],
+        postalCode: ['',[Validators.required]],
         state: [null,[Validators.required]],
         city: [null,[Validators.required]],
       })
