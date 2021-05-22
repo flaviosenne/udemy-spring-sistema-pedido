bcf92dbbd19c2331c9d55f2075a380eecd435b1f
Joao flavio <flaviosenne123@gmail.com>
2021-05-22 18:59:57 -0300

    add swagger and connection mysql local

--- back-end/src/main/java/com/ordering/system/config/SecurityConfig.java
+++ back-end/src/main/java/com/ordering/system/config/SecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -49,6 +50,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         "/products/**",
         "/categories/**"
     };
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers(
+                        "/v2/api-docs",
+                        "/configuration/ui",
+                        "/swagger-resources/**",
+                        "/configuration/**",
+                        "/swagger-ui.html",
+                        "/webjars/**");
+    }
+
     @Override
     protected void configure(HttpSecurity http)throws Exception{
         if(Arrays.asList(env.getActiveProfiles()).contains("test")){
